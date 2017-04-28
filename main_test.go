eac4cde1744fb017bb7139158ea42943302f7ce9
Alexander Menzhinsky <amenzhinsky@gmail.com>
2017-04-28 18:51:40 +0300

    Add main tests

--- main_test.go
+++ main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -29,7 +28,18 @@ func TestFlow(t *testing.T) {
 	h.HandleFunc("/metrics/find", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`[{"id": "foo.bar.baz"}, {"id": "bar.baz"}]`))
+		w.Write([]byte(`[
+			{"id": "foo.bar.baz", "isLeaf": 1},
+			{"id": "bar.baz.bam", "isLeaf": 0}
+		]`))
+	})
+	h.HandleFunc("/render", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`[
+			{"target": "foo.*", "datapoints": [[1, 0]]},
+			{"target": "bar.baz.bam", "datapoints": [[0, 1]]
+		}]`))
 	})
 
 	b := httptest.NewServer(h)
@@ -47,7 +57,7 @@ func TestFlow(t *testing.T) {
 	r := httptest.NewRequest("POST", "/users", strings.NewReader(`{
 		"username": "user",
 		"password": "secret",
-		"globs":    ["foo.*"]
+		"globs":    ["foo.*", "bar.baz"]
 	}`))
 	r.SetBasicAuth("admin", "secret")
 	uh.ServeHTTP(w, r)
@@ -55,13 +65,20 @@ func TestFlow(t *testing.T) {
 		t.Errorf("create user code = %d, want %d", w.Code, http.StatusOK)
 	}
 
-	w = httptest.NewRecorder()
-	r = httptest.NewRequest("GET", "/metrics/find?query=foo.*", nil)
-	r.SetBasicAuth("user", "secret")
-	ch.ServeHTTP(w, r)
-	if w.Code != http.StatusOK {
-		t.Errorf("carbonapi request code = %d, want %d", w.Code, http.StatusOK)
+	for url, body := range map[string]string{
+		"/metrics/find?query=foo.*":        `[{"id":"foo.bar.baz","isLeaf":1}]`,
+		"/render?target=foo.*&format=json": `[{"target":"foo.*","datapoints":[[1,0]]}]`,
+	} {
+		t.Run(url, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest("GET", url, nil)
+			r.SetBasicAuth("user", "secret")
+			ch.ServeHTTP(w, r)
+			if w.Code != http.StatusOK {
+				t.Errorf("GET %s code = %d, want %d", url, w.Code, http.StatusOK)
+			} else if w.Body.String() != body {
+				t.Errorf("GET %s body = %q, want %q", url, w.Body.String(), body)
+			}
+		})
 	}
-
-	fmt.Println(w.Body.String())
 }
